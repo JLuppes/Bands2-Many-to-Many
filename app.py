adcc7e6e55b4bda36711e56d483886adfb203998
Joshua Luppes <lupjos@dunwoody.edu>
2026-04-16 10:21:21 -0500

    Refactor error messages to use toasts instead
    Also added missing try-except for db operations, and pulled form info out from db object creations.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from model import db, Bands, Members, Albums, Memberships, admin
@@ -57,37 +57,58 @@ def view_band(id):
 
 @app.route('/bands/add', methods=['GET', 'POST'])
 def add_band():
+
     if request.method == 'POST':
-        new_band = Bands(
-            BandName=request.form['bandname'],
-            FormedYear=request.form['formedyear'],
-            HomeLocation=request.form['homelocation']
-        )
-        db.session.add(new_band)
-        db.session.commit()
-        return redirect(url_for('view_by_band'))
+
+        try:
+
+            band_name = request.form['bandname']
+            formed_year = request.form['formedyear']
+            home_location = request.form['homelocation']
+
+            new_band = Bands(
+                BandName=band_name,
+                FormedYear=formed_year,
+                HomeLocation=home_location
+            )
+
+            db.session.add(new_band)
+            db.session.commit()
+
+            flash(f'Added band: {band_name}', 'success')
+
+            return redirect(url_for('view_by_band'))
+
+        except Exception as e:
+            db.session.rollback()
+            flash('Error adding band!', 'danger')
+
     return render_template('add_band.html')
 
 
 @app.route('/bands/edit/<int:id>', methods=['GET', 'POST'])
 def edit_band(id):
+
     band = Bands.query.get_or_404(id)
 
     if request.method == 'POST':
+
         try:
 
-            band.Name = request.form.get('bandname')
+            band.BandName = request.form.get('bandname')
             band.FormedYear = request.form.get('formedyear')
             band.HomeLocation = request.form.get('homelocation')
 
             db.session.add(band)
             db.session.commit()
+
+            flash(f"Updated info for band: {band.BandName}", 'info')
+
             return redirect(url_for('view_by_band'))
 
         except Exception as e:
             db.session.rollback()
-            error = f"Error updating band: {e}"
-            return render_template('edit_band.html', band=band, error=error)
+            flash(f'Error updating band: {band.BandName}', 'danger')
 
     return render_template('edit_band.html', band=band)
 
@@ -95,20 +116,25 @@ def edit_band(id):
 @app.route('/bands/delete/<int:id>')
 def delete_band(id):
     band = Bands.query.get_or_404(id)
+    band_name = band.BandName
 
     try:
         db.session.delete(band)
         db.session.commit()
+
+        flash(f'Deleted band: {band_name}')
         return redirect(url_for('view_by_band'))
+
     except Exception as e:
         db.session.rollback()
-        error = f"Error deleting band: {e}"
+        flash(f"Error deleting band: {band_name}", 'danger')
         return redirect(url_for('view_by_band'))
 
 
 # ==========================
 # MEMBERS
 # ==========================
+
 
 @app.route('/members/view')
 def view_by_member():
@@ -126,7 +152,9 @@ def view_member(id):
 
 @app.route('/members/add', methods=['GET', 'POST'])
 def add_member():
-    bands = Bands.query.all()  # Students see querying with relationships
+
+    bands = Bands.query.all()
+
     if request.method == 'POST':
         new_member = Members(
             MemberName=request.form['membername'],
@@ -134,25 +162,35 @@ def add_member():
         )
         db.session.add(new_member)
         db.session.commit()
+
+        flash(f"Added member: {new_member.MemberName}", 'success')
+
         return redirect(url_for('view_by_member'))
+
     return render_template('add_member.html', bands=bands)
 
 
 @app.route('/members/edit/<int:id>', methods=['GET', 'POST'])
 def edit_member(id):
+
     if request.method == 'POST':
+
         member = Members.query.get_or_404(request.form.get('memberid'))
         try:
-            member.MemberName = request.form.get('membername')
-            member.MainPosition = request.form.get('mainposition')
+            member_name = request.form.get('membername')
+            main_position = request.form.get('mainposition')
+            member.MemberName = member_name
+            member.MainPosition = main_position
 
             db.session.add(member)
             db.session.commit()
+
+            flash(f'Updated info for member: {member_name}', 'info')
             return redirect(url_for('view_by_member'))
         except Exception as e:
             db.session.rollback()
-            error = f"Error editing member: {e}"
-            return render_template('edit_member.html', member=member, error=error)
+            flash(f'Error editing member: {member_name}', 'danger')
+
     member = Members.query.get_or_404(id)
     return render_template('edit_member.html', member=member)
 
@@ -160,15 +198,18 @@ def edit_member(id):
 @app.route('/members/delete/<int:id>')
 def delete_member(id):
     member = Members.query.get_or_404(id)
+    member_name = member.MemberName
     try:
         db.session.delete(member)
         db.session.commit()
+
+        flash(f'Deleted member: {member_name}', 'info')
         return redirect(url_for('view_by_member'))
     except Exception as e:
         db.session.rollback()
-        error = f"Error deleting member: {e}"
+        flash(f'Error deleting member: {member_name}', 'danger')
         members = Members.query.all()
-        return render_template('dsiplay_by_member.html', members=members, error=error)
+        return render_template('dsiplay_by_member.html', members=members)
 
 
 # ==========================
@@ -181,21 +222,32 @@ def add_membership():
     members = Members.query.all()
     if request.method == 'POST':
         try:
+            band_id = request.form.get('bandid')
+            member_id = request.form.get('memberid')
+            role = request.form.get('role')
+            start_year = request.form.get('startyear') or None
+            end_year = request.form.get('endyear') or None
+
+            member = Members.query.get_or_404(member_id)
+            band = Bands.query.get_or_404(band_id)
+
             membership = Memberships(
-                BandID=request.form.get('bandid'),
-                MemberID=request.form.get('memberid'),
-                Role=request.form.get('role'),
-                StartYear=request.form.get('startyear') or None,
-                EndYear=request.form.get('endyear') or None
+                BandID=band_id,
+                MemberID=member_id,
+                Role=role,
+                StartYear=start_year,
+                EndYear=end_year
             )
             db.session.add(membership)
             db.session.commit()
-            # flash('Membership assigned', 'success')
+
+            flash(
+                f'Added membership for {member.MemberName} in {band.BandName}', 'success')
             return redirect(url_for('view_by_band'))
         except Exception as e:
             db.session.rollback()
-            error = f"Error adding new membership: {e}"
-            return render_template('add_membership.html', bands=bands, members=members, error=error)
+            flash(
+                f'Error adding membership for {member.MemberName} in {band.BandName}', 'danger')
     return render_template('add_membership.html', bands=bands, members=members)
 
 
@@ -205,24 +257,51 @@ def edit_membership(id):
     bands = Bands.query.all()
     members = Members.query.all()
     if request.method == 'POST':
-        membership.MembershipID = request.form.get('membership_id')
-        membership.BandID = request.form.get('bandid')
-        membership.MemberID = request.form.get('memberid')
-        membership.Role = request.form.get('role')
-        membership.StartYear = request.form.get('startyear') or None
-        membership.EndYear = request.form.get('endyear') or None
-        db.session.commit()
-        # flash('Membership updated', 'success')
-        return redirect(url_for('view_by_band'))
+
+        try:
+
+            membership_id = request.form.get('membership_id')
+            membership_band_id = request.form.get('bandid')
+            membership_member_id = request.form.get('memberid')
+            membership_role = request.form.get('role')
+            membership_start_year = request.form.get('startyear') or None
+            membership_end_year = request.form.get('endyear') or None
+
+            band = Bands.query.get_or_404(membership_band_id)
+            member = Members.query.get_or_404(membership_member_id)
+
+            membership.MembershipID = membership_id
+            membership.BandID = membership_band_id
+            membership.MemberID = membership_member_id
+            membership.Role = membership_role
+            membership.StartYear = membership_start_year
+            membership.EndYear = membership_end_year
+
+            db.session.commit()
+            flash(
+                f'Updated membership for member {member.MemberName} in band {band.BandName}', 'info')
+            return redirect(url_for('view_by_band'))
+        except Exception as e:
+            db.session.rollback()
+            flash(
+                f'Error editing membership for {member.MemberName} in {band.BandName}', 'danger')
     return render_template('edit_membership.html', membership=membership, bands=bands, members=members)
 
 
 @app.route('/memberships/delete/<int:id>')
 def delete_membership(id):
+
     membership = Memberships.query.get_or_404(id)
-    db.session.delete(membership)
-    db.session.commit()
-    # flash('Membership removed', 'success')
+
+    try:
+        db.session.delete(membership)
+        db.session.commit()
+        flash('Membership removed', 'success')
+
+    except Exception as e:
+        db.session.rollback()
+        flash('Error removing membership', 'danger')
+
     return redirect(url_for('view_by_band'))
 
 
@@ -234,14 +313,23 @@ def delete_membership(id):
 def add_album():
     bands = Bands.query.all()
     if request.method == 'POST':
-        new_album = Albums(
-            AlbumTitle=request.form['albumtitle'],
-            ReleaseYear=request.form['releaseyear'],
-            BandID=request.form['bandid']
-        )
-        db.session.add(new_album)
-        db.session.commit()
-        return redirect(url_for('view_by_band'))
+        try:
+            album_title = request.form['albumtitle']
+            release_year = request.form['releaseyear']
+            band_id = request.form['bandid']
+
+            new_album = Albums(
+                AlbumTitle=album_title,
+                ReleaseYear=release_year,
+                BandID=band_id
+            )
+            db.session.add(new_album)
+            db.session.commit()
+            flash(f'Added album: {album_title}', 'success')
+            return redirect(url_for('view_by_band'))
+        except Exception as e:
+            db.session.rollback()
+            flash('Error adding album', 'danger')
     return render_template('add_album.html', bands=bands)
 
 
